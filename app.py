e7aacea594782a58843d9910de74fd4d82c9fa3b
Ashwinkumar E <Ashwinkumar.E@STLERI.COM>
2022-08-29 15:43:51 +0530

    4th commit

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def login_doctor():
             return jsonify('Logged in')
         else:
             return jsonify('Incorrect username/password!')
-    return " User got logged in successfully!"
+    return "user logged in successfully!"
 
 @app.route("/logout")
 def logout_doctor():
